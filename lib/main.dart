963ec6d027280c88629105f529ddefb2d89a2b67
heosangbeom <luv_3_u@naver.com>
2024-12-22 02:11:10 +0900

    feat(render): 텍스트 렌더링
    - 텍스트 렌더링
       - 배경 컬러
       - 텍스트 컬러

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,16 @@ void main() {
   runApp(
     // MaterialApp 은 항상 최상위에 위치한다.
     // Scaffold 는 바로 아래에 위치한다.
-    MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Test'),
+    // 위젯 -> widget ( 화면에 보이게 하는 요소를 위젯이라 부름 )
+    MaterialApp(// material 디자인을 사용할 수 있게 하는 위젯
+      // debugShowCheckedModeBanner: false,
+      home: Scaffold(// 화면에 구조를 만들 수 있게 하는 위젯
+        backgroundColor: Colors.black,
+        body: Center(// 가운데 정렬하는 위젯
+          child: Text(
+            'Test',
+            style: TextStyle(color: Colors.white),
+          ), // 글자를 보여주는 위젯
         ),
       ),
     ),
